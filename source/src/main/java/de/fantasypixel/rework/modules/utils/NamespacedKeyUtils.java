adc0959dfc35717cf6f9c597f4c1d4080229d14b
M a n u u <manuel.rettowski@gmail.com>
2024-05-22 16:59:22 +0200

    + NamespacedKeyUtils

---
+++ source/src/main/java/de/fantasypixel/rework/modules/utils/NamespacedKeyUtils.java
@@ -0,0 +1,36 @@
+package de.fantasypixel.rework.modules.utils;
+
+import de.fantasypixel.rework.framework.provider.Auto;
+import de.fantasypixel.rework.framework.provider.ServiceProvider;
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.Plugin;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Provides easy utility access to namespaced-keys.
+ */
+@ServiceProvider
+public class NamespacedKeyUtils {
+
+    public enum NamespacedKeyType { INVENTORY_ITEM_ID }
+
+    @Auto private Plugin plugin;
+
+    private final Map<NamespacedKeyType, NamespacedKey> keyMap;
+
+    public NamespacedKeyUtils() {
+        this.keyMap = new HashMap<>();
+    }
+
+    @Nonnull
+    public NamespacedKey getNamespacedKey(NamespacedKeyType key) {
+        if (!keyMap.containsKey(key))
+            keyMap.put(key, new NamespacedKey(this.plugin, key.name()));
+
+        return keyMap.get(key);
+    }
+
+}
